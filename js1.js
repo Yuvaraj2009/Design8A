562ede708e84c942c45d8770468b8c5f81444a3c
Yuvaraj <151709666+Yuvaraj2009@users.noreply.github.com>
2023-11-22 11:01:17 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,12 @@
+﻿
+function sayHello() {
+alert("Thank you for submitting.You are our first responsibility. Our staff will respond as soon as possible.")
+}
+
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
